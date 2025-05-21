0aea989a10fa95a4549a1128943cf1f525460792
Carlos Borges <carloos-boorges@hotmail.com>
2025-05-20 23:31:20 -0300

    feat: adiciona endpoints de CRUD para Aluno

--- src/main/java/com/borges/curso_api/controller/AlunoController.java
+++ src/main/java/com/borges/curso_api/controller/AlunoController.java
@@ -1,4 +1,40 @@
 package com.borges.curso_api.controller;
+import com.borges.curso_api.entity.Aluno;
+import com.borges.curso_api.service.AlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/alunos")
 public class AlunoController {
+
+    @Autowired
+    private AlunoService alunoService;
+
+    @PostMapping
+    public ResponseEntity<Aluno> salvar(@RequestBody Aluno aluno){
+        Aluno salvo = alunoService.salvar(aluno);
+        return ResponseEntity.ok(salvo);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Aluno>> listarTodos() {
+        return ResponseEntity.ok(alunoService.listarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Aluno> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(alunoService.buscarPorId(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable Long id) {
+        alunoService.excluir(id);
+        return ResponseEntity.noContent().build();
+    }
 }
+
+
